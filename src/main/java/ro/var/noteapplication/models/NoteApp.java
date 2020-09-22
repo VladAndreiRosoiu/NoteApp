c6407a4f935a08d4efc2875d141324364aa96139
VladAndreiRosoiu <rosoiuandreivlad@gmail.com>
2020-09-22 20:27:38 +0300

    implemented entire functionality. Added try catch where needed!

--- src/main/java/ro/var/noteapplication/models/NoteApp.java
+++ src/main/java/ro/var/noteapplication/models/NoteApp.java
@@ -29,27 +29,21 @@ public class NoteApp {
                 int option = scanner.nextInt();
                 switch (option) {
                     case 1:
-                        //TODO - try catch
                         addNewNote();
-                        ioService.writeFile(noteList, notesFile);
                         break;
                     case 2:
-                        //TODO - try catch
-                        displayNotes();
+                        displayOptions();
                         break;
                     case 3:
-                        //TODO - search note
+                        selectNote(Objects.requireNonNull(searchNotes()));
+                        break;
                     case 4:
-                        //TODO - try catch
-                        editNote();
-                        ioService.writeFile(noteList, notesFile);
+                        editNote(selectNote(Objects.requireNonNull(searchNotes())));
                         break;
                     case 5:
-                        //TODO - try catch
                         System.exit(0);
                         break;
                     default:
-                        //TODO - try catch
                         System.out.println("Please choose between [1-4] only!");
                         openNoteApp();
                         break;
@@ -94,8 +88,8 @@ public class NoteApp {
         System.out.println("2 - Edit body");
         System.out.println("3 - Edit hashtag list");
         System.out.println("4 - Mark note as finished");
-        System.out.println("4 - Delete note");
-        System.out.println("5 - Return to main menu");
+        System.out.println("5 - Delete note");
+        System.out.println("6 - Return to main menu");
         System.out.println("Please enter an option:");
     }
 
@@ -116,40 +110,51 @@ public class NoteApp {
         String body = scanner.nextLine();
         System.out.println("Please enter hashtag/s:");
         String hashtag = scanner.nextLine();
-        List<String> hashtagList = new ArrayList<>(Arrays.asList(hashtag.split("#")));
+        List<String> hashtagList = new ArrayList<>();
+        if (hashtag.contains("#")) {
+            String[] arrayHashtagList = hashtag.split("#");
+            for (int i = 0; i < arrayHashtagList.length; i++) {
+                arrayHashtagList[i] = "#" + arrayHashtagList[i];
+                hashtagList.add(arrayHashtagList[i]);
+            }
+            hashtagList.remove(0);
+        } else {
+            System.out.println("Invalid input!");
+        }
         noteList.add(new Note(title, body, System.currentTimeMillis(), System.currentTimeMillis(), false, hashtagList));
-
-        //TODO - fix adding hashtags
-
-//        String[] hashtags = input.nextLine().replace(" ", "").split(",");
-//        for (int i = 0; i < hashtags.length; i++) {
-//        hashtags[i] = "#" + hashtags[i];
-//        }
+        System.out.println("Note successfully added!");
+        ioService.writeFile(noteList, notesFile);
     }
 
-    private void displayNotes() {
-        printNoteAppDisplayMenu();
-        int option = scanner.nextInt();
-        switch (option) {
-            case 1:
-                printNotes(sortingService.latestNotesFirst(noteList));
-                break;
-            case 2:
-                printNotes(sortingService.oldestNotesFirst(noteList));
-                break;
-            case 3:
-                printNotes(sortingService.latestEditedNotesFirst(noteList));
-                break;
-            case 4:
-                printNotes(sortingService.oldestEditedNotesFirst(noteList));
-                break;
-            case 5:
-                printNotes(sortingService.unfinishedNotes(noteList));
-            case 6:
-                openNoteApp();
-                break;
-            default:
-                System.out.println("Error");
+    private void displayOptions() {
+        try {
+            printNoteAppDisplayMenu();
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    printNotes(sortingService.latestNotesFirst(noteList));
+                    break;
+                case 2:
+                    printNotes(sortingService.oldestNotesFirst(noteList));
+                    break;
+                case 3:
+                    printNotes(sortingService.latestEditedNotesFirst(noteList));
+                    break;
+                case 4:
+                    printNotes(sortingService.oldestEditedNotesFirst(noteList));
+                    break;
+                case 5:
+                    printNotes(sortingService.unfinishedNotes(noteList));
+                case 6:
+                    openNoteApp();
+                    break;
+                default:
+                    System.out.println("Invalid input. Please choose between [1-6] only!");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please choose between [1-6] only!");
+            scanner = new Scanner(System.in);
+            displayOptions();
         }
     }
 
@@ -174,13 +179,124 @@ public class NoteApp {
         for (Note note : noteList) {
             for (String hashtag : note.getHashtagList()) {
                 if (hashtag.toLowerCase().contains(keyword.toLowerCase())) {
-                    if (!tempList.contains(note)){
+                    if (!tempList.contains(note)) {
                         tempList.add(note);
                     }
                 }
             }
         }
         return tempList;
+    }
+
+    private List<Note> searchNotes() {
+        try {
+            printSearchMenu();
+            int option = scanner.nextInt();
+            String keyword;
+            switch (option) {
+                case 1:
+                    scanner.skip("\n");
+                    System.out.println("Please enter title to search");
+                    keyword = scanner.nextLine();
+                    return searchNoteByTitle(keyword);
+                case 2:
+                    scanner.skip("\n");
+                    System.out.println("Please enter hashtag to search");
+                    keyword = scanner.nextLine();
+                    return searchNoteByHashtag(keyword);
+                case 3:
+                    scanner.skip("\n");
+                    System.out.println("Please enter keyword to search");
+                    keyword = scanner.nextLine();
+                    return searchNoteByBodyKeyword(keyword);
+                case 4:
+                    openNoteApp();
+                default:
+                    System.out.println("Invalid input. Please choose between [1-4] only!");
+            }
+            return null;
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please choose between [1-4] only!");
+            scanner = new Scanner(System.in);
+            searchNotes();
+            return null;
+        }
+    }
+
+    private Note selectNote(List<Note> notes) {
+        for (int i = 0; i < notes.size(); i++) {
+            System.out.println((i + 1) + " - " + notes.get(i));
+        }
+        System.out.println("Please select one note:");
+        int selectedNote = scanner.nextInt();
+        if (selectedNote >= 0 && selectedNote <= notes.size()) {
+            System.out.println("Selected note is:\n" + notes.get(selectedNote - 1));
+            return notes.get(selectedNote - 1);
+        } else {
+            System.out.println("Invalid input!");
+            return null;
+        }
+    }
+
+    private void editNote(Note currentNote) {
+        try {
+            if (currentNote != null) {
+                printNoteEditMenu();
+                int option = scanner.nextInt();
+                String keyword;
+                switch (option) {
+                    case 1:
+                        scanner.skip("\n");
+                        System.out.println("Please enter new title");
+                        keyword = scanner.nextLine();
+                        editNoteTitle(currentNote, keyword);
+                        break;
+                    case 2:
+                        scanner.skip("\n");
+                        System.out.println("Please enter new body");
+                        keyword = scanner.nextLine();
+                        editNoteBody(currentNote, keyword);
+                        break;
+                    case 3:
+                        scanner.skip("\n");
+                        System.out.println("Please enter hashtags");
+                        keyword = scanner.nextLine();
+                        editHashtag(currentNote, keyword);
+                        break;
+                    case 4:
+                        System.out.println("Do you want to mark current note as finished? Y/N:");
+                        String answer = scanner.next();
+                        if (answer.equalsIgnoreCase("y")) {
+                            currentNote.setMarkAsFinished(true);
+                        } else {
+                            System.out.println("Nothing has changed, note has not been marked as finished!");
+                        }
+                        break;
+                    case 5:
+                        System.out.println("Do you want to delete current note? Y/N:");
+                        String deleteAnswer = scanner.next();
+                        if (deleteAnswer.equalsIgnoreCase("y")) {
+                            noteList.remove(currentNote);
+                            System.out.println("Note successfully deleted!");
+                        } else {
+                            System.out.println("Nothing has changed, note has not been deleted!");
+                        }
+                        break;
+                    case 6:
+                        openNoteApp();
+                        break;
+                    default:
+                        System.out.println("Invalid input. Please choose between [1-6] only!");
+                }
+            } else {
+                System.out.println("Invalid note!");
+            }
+            ioService.writeFile(noteList, notesFile);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please choose between [1-6] only!");
+            scanner = new Scanner(System.in);
+            editNote(currentNote);
+        }
     }
 
     private void editNoteTitle(Note note, String title) {
@@ -194,85 +310,19 @@ public class NoteApp {
     }
 
     private void editHashtag(Note note, String hashtag) {
-        List<String> hashtags = new ArrayList<>(Arrays.asList(hashtag.split("#")));
-        note.setHashtagList(hashtags);
+        note.getHashtagList().removeAll(note.getHashtagList());
+        List<String> hashtagList = new ArrayList<>();
+        if (hashtag.contains("#")) {
+            String[] arrayHashtagList = hashtag.split("#");
+            for (int i = 0; i < arrayHashtagList.length; i++) {
+                arrayHashtagList[i] = "#" + arrayHashtagList[i];
+                hashtagList.add(arrayHashtagList[i]);
+            }
+            hashtagList.remove(0);
+        } else {
+            System.out.println("Invalid input!");
+        }
+        note.setHashtagList(hashtagList);
         note.setModifiedOnDate(System.currentTimeMillis());
     }
-
-    private Note selectNote(List<Note> notes) {
-        for (int i = 0; i < notes.size(); i++) {
-            System.out.println((i + 1) + " - " + notes.get(i));
-        }
-        System.out.println("Please select one note:");
-        int selectedNote = scanner.nextInt();
-        return notes.get(selectedNote - 1);
-    }
-
-    private List<Note> searchNotes() {
-        printSearchMenu();
-        int option = scanner.nextInt();
-        String keyword;
-        switch (option) {
-            case 1:
-                scanner.skip("\n");
-                System.out.println("Please enter title to search");
-                keyword = scanner.nextLine();
-                return searchNoteByTitle(keyword);
-            case 2:
-                scanner.skip("\n");
-                System.out.println("Please enter hashtag to search");
-                keyword = scanner.nextLine();
-                return searchNoteByHashtag(keyword);
-            case 3:
-                scanner.skip("\n");
-                System.out.println("Please enter keyword to search");
-                keyword = scanner.nextLine();
-                return searchNoteByBodyKeyword(keyword);
-            case 4:
-                openNoteApp();
-            default:
-                System.out.println("default");
-        }
-        return null;
-    }
-
-    private void editNote() {
-        Note currentNote = selectNote(Objects.requireNonNull(searchNotes()));
-        System.out.println("Selected note is:" + currentNote);
-        printNoteEditMenu();
-        int option = scanner.nextInt();
-        String keyword;
-        switch (option) {
-            case 1:
-                scanner.skip("\n");
-                System.out.println("Please enter new title");
-                keyword = scanner.nextLine();
-                editNoteTitle(currentNote, keyword);
-                break;
-            case 2:
-                scanner.skip("\n");
-                System.out.println("Please enter new body");
-                keyword = scanner.nextLine();
-                editNoteBody(currentNote, keyword);
-                break;
-            case 3:
-                scanner.skip("\n");
-                System.out.println("Please enter hashtags");
-                keyword = scanner.nextLine();
-                editHashtag(currentNote, keyword);
-                break;
-            case 4:
-                //TODO - mark note as finished
-                break;
-            case 5:
-                //TODO - delete note
-                break;
-            case 6:
-                openNoteApp();
-                break;
-            default:
-                System.out.println("Error");
-        }
-    }
-
 }
